33b380d7bc96a78b47222be58fde0a97a9bff360
Adam Hill <adam.hill@outlook.com>
2016-11-08 13:25:15 +0000

    Hand win evaluator.

---
+++ Hearts/Scoring/HandWinEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hearts.Model;
+
+namespace Hearts.Scoring
+{
+    public class HandWinEvaluator
+    {
+        public Guid EvaluateWinner(PlayedHand playedHand)
+        {
+            var leadSuit = playedHand.Cards.First().Value.Suit;
+            var cardsOfLeadSuit = playedHand.Cards.Where(i => i.Value.Suit == leadSuit);
+            var highestRanked = cardsOfLeadSuit.OrderByDescending(i => i.Value.Kind).First();
+
+            return highestRanked.Key;
+        }
+    }
+}
