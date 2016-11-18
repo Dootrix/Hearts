c956cf2cdd45a496aa4efe23780a06ed17843cbe
Adam Hill <adam.hill@outlook.com>
2016-11-18 19:08:34 +0000

    Beautified comments.

--- Hearts.Console/Settings.cs
+++ Hearts.Console/Settings.cs
@@ -7,18 +7,19 @@ namespace Hearts.Console
     public class Settings
     {
         // Note: This simulates at about 300 games per second, without console outputs
-        // 1 game - v. fast
-        // 1,000 games - 3 seconds
-        // 10,000 games - 30 seconds
-        public static int GameSimulationCount = 10000;
+        //      1 game  - 0s
+        //  1,000 games - 3s
+        // 10,000 games - 30s
+        public static int GameSimulationCount = 1000;
         public static bool ShowFullOutput = false;
         public static List<Player> Bots = new List<Player>
             {
-                // Available bots in comments...
-                // TerribleRandomAiAgent
-                // Noob1AiExampleAgent
-                // Noob2AiExampleAgent
-                // BeastAi.SavageBeast
+                // Available bots:
+                //    • TerribleRandomAiAgent
+                //    • Noob1AiExampleAgent
+                //    • Noob2AiExampleAgent
+                //    • SavageBeast (Adam Hill)
+                //    • NoobCrusher (Tony Beasley)
                 new Player("A", new SavageBeast()),
                 new Player("B", new Noob2AiExampleAgent()),
                 new Player("C", new Noob2AiExampleAgent()),
