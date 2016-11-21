b44235eb85d034c7c0fe36e5594d481040582baa
Adam Hill <adam.hill@outlook.com>
2016-11-21 19:53:54 +0000

    Added PlayerHolding concept.

---
+++ Hearts/Model/PlayerHolding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearts.Model
+{
+    public class PlayerHolding
+    {
+        public IEnumerable<Card> StartingHands { get; set; }
+        public IEnumerable<Card> PassedCards { get; set; }
+        public IEnumerable<Card> PostPassHands { get; set; }
+        public IEnumerable<Card> RemainingCards { get; set; }
+        public IEnumerable<Card> LegalCards { get; set; }
+    }
+}
