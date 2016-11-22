924f41c7e75afce50cd1a80c2544ae6ca1ec48b9
Adam Hill <adam.hill@outlook.com>
2016-11-22 16:31:45 +0000

    Beast converted to new IAgent signature.

--- Hearts.Console/Settings.cs
+++ Hearts.Console/Settings.cs
@@ -20,12 +20,12 @@ namespace Hearts.Console
                 //    • Noob1AiExampleAgent
                 //    • Noob2AiExampleAgent
                 //    • Noob3AiExampleAgent
-                //    • SavageBeast (Adam Hill) *
+                //    • SavageBeast (Adam Hill)
                 //    • NoobCrusher.Create(NoobCrusherVersion.v1) (Tony Beasley)
                 //    • NoobCrusher.Create(NoobCrusherVersion.v2) (Tony Beasley)
                 new Player("A", new Noob1AiExampleAgent()),
-                new Player("B", new Noob2AiExampleAgent()),
-                new Player("C", new Noob3AiExampleAgent()),
+                new Player("B", new Noob3AiExampleAgent()),
+                new Player("C", new SavageBeast(true)),
                 new Player("D", NoobCrusher.Create(NoobCrusherVersion.v2))
             };
     }
