e2644a93a766e83006f3d2135ba81c126517b9ce
Adam Hill <adam.hill@outlook.com>
2016-11-22 14:04:32 +0000

    Changed default Settings.

--- Hearts.Console/Settings.cs
+++ Hearts.Console/Settings.cs
@@ -10,16 +10,17 @@ namespace Hearts.Console
         //      1 game  - 0s
         //  1,000 games - 3s
         // 10,000 games - 30s
-        public static int GameSimulationCount = 100;
-        public static bool ShowFullOutput = false;
+        public static int GameSimulationCount = 1;
+        public static bool ShowFullOutput = GameSimulationCount == 1;
         public static List<Player> Bots = new List<Player>
             {
                 // Available bots:
+                // (* = Doesn't currently build against current IAgent signature)
                 //    • TerribleRandomAiAgent
                 //    • Noob1AiExampleAgent
                 //    • Noob2AiExampleAgent
                 //    • Noob3AiExampleAgent
-                //    • SavageBeast (Adam Hill)
+                //    • SavageBeast (Adam Hill) *
                 //    • NoobCrusher.Create(NoobCrusherVersion.v1) (Tony Beasley)
                 //    • NoobCrusher.Create(NoobCrusherVersion.v2) (Tony Beasley)
                 new Player("A", new Noob1AiExampleAgent()),
