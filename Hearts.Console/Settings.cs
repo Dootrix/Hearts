48b1d5027613b81b5f33243bded3fc6f70cb4ded
Tony Beasley <tony.beasley@gmail.com>
2016-11-24 06:42:37 +0000

    NoobCrusher - improved strategy.

--- Hearts.Console/Settings.cs
+++ Hearts.Console/Settings.cs
@@ -11,7 +11,7 @@ namespace Hearts.Console
         //      1 game  - 0s
         //  1,000 games - 3s
         // 10,000 games - 30s
-        public static int GameSimulationCount = 1000;
+        public static int GameSimulationCount = 100;
         public static bool ShowFullOutput = GameSimulationCount == 1;
         public static List<Bot> Bots = new List<Bot>
             {
@@ -25,10 +25,10 @@ namespace Hearts.Console
                 //    • NoobCrusher.Create(NoobCrusherVersion.v1) (Tony Beasley)
                 //    • NoobCrusher.Create(NoobCrusherVersion.v2) (Tony Beasley)
                 
-                Bot.Create(new Player("A"), new SavageBeast(false)),
+                Bot.Create(new Player("A"), new SavageBeast(true)),
                 Bot.Create(new Player("B"), NoobCrusher.Create(NoobCrusherVersion.v2)),
-                Bot.Create(new Player("C"), new Noob3AiExampleAgent()),
-                Bot.Create(new Player("D"), new QueenCatcherAI.QueenCatcher())
+                Bot.Create(new Player("C"), new QueenCatcherAI.QueenCatcher()),
+                Bot.Create(new Player("D"), new Noob3AiExampleAgent())
             };
     }
 }
