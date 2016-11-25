eb4df62488459a1eb2aebb4d7f3332b4297277ed
Craig Rowe <craig.rowe@dootrix.com>
2016-11-25 10:03:18 +0000

    PlayedCard extensions (plus tests)

--- Hearts/Extensions/PlayedCardExtensions.cs
+++ Hearts/Extensions/PlayedCardExtensions.cs
@@ -10,5 +10,27 @@ namespace Hearts.Extensions
         {
             return self.Select(i => i.Card);
         }
+
+        public static Suit? LeadSuit(this IEnumerable<PlayedCard> self)
+        {
+            var lead = self.FirstOrDefault();
+            if (lead == null)
+            {
+                return null;
+            }
+
+            return lead.Card.Suit;
+        }
+
+        public static Card WinningCard(this IEnumerable<PlayedCard> self)
+        {
+            var leadSuit = self.LeadSuit();
+            if (leadSuit.HasValue)
+            {
+                return self.SelectCards().OfSuit(leadSuit.Value).Descending().First();
+            }
+
+            return null;
+        }
     }
 }
