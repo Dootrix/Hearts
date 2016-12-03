f56b4498615735d5eab7fdb9abc12067d0564256
Tony Beasley <tony.beasley@gmail.com>
2016-12-03 11:02:48 +0000

    Performance improvements.

--- Hearts/Model/GameState.cs
+++ Hearts/Model/GameState.cs
@@ -5,23 +5,22 @@ namespace Hearts.Model
 {
     public class GameState
     {
-        public GameState(Player player, Game game, PlayerState cards)
+        private readonly PlayerState playerState;
+
+        public GameState(Player player, Game game, PlayerState playerState)
         {
             this.Player = player;
             this.Game = game;
-            this.StartingCards = cards.Starting.ToList();
-            this.PostPassCards = cards.PostPass != null ? cards.PostPass.ToList() : Enumerable.Empty<Card>();
-            this.CurrentCards = cards.Current != null ? cards.Current.ToList() : Enumerable.Empty<Card>();
-            this.LegalCards = cards.Legal != null ? cards.Legal.ToList() : Enumerable.Empty<Card>();
+            this.playerState = playerState;
         }
 
         public Player Player { get; private set; }
 
         public Game Game { get; private set; }
 
-        public IEnumerable<Card> StartingCards { get; private set; }
-        public IEnumerable<Card> PostPassCards { get; private set; }
-        public IEnumerable<Card> CurrentCards { get; private set; }
-        public IEnumerable<Card> LegalCards { get; private set; }
+        public IEnumerable<Card> StartingCards { get { return this.playerState.Starting; }  }
+        public IEnumerable<Card> PostPassCards { get { return this.playerState.PostPass; } }
+        public IEnumerable<Card> CurrentCards { get { return this.playerState.Current; } }
+        public IEnumerable<Card> LegalCards { get { return this.playerState.Legal; } }
     }
 }
